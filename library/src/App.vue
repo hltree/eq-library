5a657428b4f564046c43893c5a02573bbdabaeff
hltree <kiona.n31@icloud.com>
2020-01-22 00:33:54 +0900

    update: delete defaultComponent

--- library/src/App.vue
+++ library/src/App.vue
@@ -1,17 +1,11 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <p>test</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
